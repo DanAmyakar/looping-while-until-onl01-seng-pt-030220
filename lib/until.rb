ab171c0b2422340dbd342ee438d0849b7aea6785
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-07 00:57:29 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -9,9 +9,13 @@
 #  
 #end
 
-levitation_force = 6
+def using_until
 
-until levitation_force == 10
-  puts "Wingardium Leviosa"
-  levitation_force = levitation_force + 1
+  levitation_force = 6
+
+  until levitation_force == 10
+    puts "Wingardium Leviosa"
+    levitation_force = levitation_force + 1
+  end
+  
 end
